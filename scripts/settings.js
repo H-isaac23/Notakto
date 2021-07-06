6c96fed08d0942800eb448bdfb0abb469d0f93a9
John Isaac Delgado <ji23delgado@gmail.com>
2021-07-06 16:39:08 +0800

    add functions

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,3 +1,5 @@
+import { lowlight, clearBoards } from "./game logic.js";
+
 if (document.readyState == "loading") {
   document.addEventListener("DOMContentLoaded", ready);
 } else {
@@ -7,15 +9,45 @@ if (document.readyState == "loading") {
 function ready() {
   let settings = document.getElementsByClassName("settings-icon")[0];
   settings.addEventListener("click", show);
+
+  let reset = document.getElementsByClassName("reset-option")[0];
+  let restart = document.getElementsByClassName("restart-option")[0];
+  let back = document.getElementsByClassName("back-option")[0];
+
+  reset.addEventListener("click", test);
+  restart.addEventListener("click", restartGame);
+  back.addEventListener("click", home);
 }
 
 function show() {
   let settingsWidth = getComputedStyle(document.body).getPropertyValue(
     "--settings-width"
   );
-  if (settingsWidth == "100%") {
+
+  if (settingsWidth == "100%" || settingsWidth == " 100%") {
     document.documentElement.style.setProperty("--settings-width", "0%");
   } else {
     document.documentElement.style.setProperty("--settings-width", "100%");
   }
+}
+
+function resetScores() {
+  let scores = document.querySelectorAll("span.score");
+  for (let i = 0; i < scores.length; i++) {
+    scores[i].innerText = "0";
+  }
+  alert("hi");
+}
+
+function restartGame() {
+  clearBoards();
+  lowlight();
+}
+
+function home() {
+  window.location.href = "../index.html";
+}
+
+function test() {
+  alert("hi");
 }
